bc8aad7bf4a6859abd9de617f66c6dfce6ed4a3b
Syed Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2023-02-12 23:13:25 +0500

    Update

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { Text } from 'react-native';
-import LinkButton from "./Screens/WelcomeScreen";
+import WelcomeScreen from "./Screens/WelcomeScreen";
 // import RegisterScreen from "./Screens/RegisterScreen";
 // import LoginScreen from "./Screens/LoginScreen";
 // import HomeScreen from "./Screens/HomeScreen";
@@ -9,7 +9,7 @@ import LinkButton from "./Screens/WelcomeScreen";
 export default function App() {
   return (
     <>
-    <LinkButton />
+    <WelcomeScreen />
       {/* <Text>Hello</Text> */}
     </>
   );
--- Screens/WelcomeScreen.js
+++ Screens/WelcomeScreen.js
@@ -2,26 +2,30 @@ import React from 'react';
 import {Text, View, Image, TouchableOpacity, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import LoginScreen from './LoginScreen';
+// import LoginScreen from './LoginScreen';
 
-function HomeScreen({navigation}) {
+function HomeScreen() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+        onPress={() => navigation.navigate('LoginScreen')}
       />
     </View>
   );
 }
 const Stack = createNativeStackNavigator();
-function WelcomeScreen() {
+function WelcomeScreen({navigation}) {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{title: 'Welcome To Sylani Online Store'}}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
